8c6938bea415d1e7bb2fe613e8d4de9b45cf5de6
Tarun Chaudhry <tarunc92@gmail.com>
2013-01-27 14:54:50 -0800

    make finders only admin functions

--- plugins/mongoose/mongoose-model.js
+++ plugins/mongoose/mongoose-model.js
@@ -48,7 +48,9 @@ module.exports = function MModel(m, manager) {
         return ret;
     });
     this.__defineGetter__('finders', function(){
-        return m.schema.statics;
+        return _u.filter(m.schema.statics, function (fn) {
+          return fn.name.indexOf('admin') == 0;
+        });
 
     });
 
